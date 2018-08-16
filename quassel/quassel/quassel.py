2b1d01d3b3eea7acdeb6803fc54c5e1a66a65442
Hannah von Reth <vonreth@kde.org>
2018-08-16 21:56:18 +0200

    No longer needed

--- quassel/quassel/quassel.py
+++ quassel/quassel/quassel.py
@@ -58,8 +58,6 @@ class Package(CMakePackageBase):
         CMakePackageBase.__init__(self)
         self.supportsNinja = self.subinfo.buildTarget == "master" or CraftVersion(self.subinfo.buildTarget) > "0.12.4"
         self.subinfo.options.configure.args = " -DUSE_QT5=ON -DCMAKE_DISABLE_FIND_PACKAGE_Qt5DBus=ON"
-        if CraftCore.compiler.isMSVC2017():
-            self.subinfo.options.configure.args += " -DCMAKE_CXX_FLAGS=\"-std:c++17\""
 
     def install(self):
         if not CMakePackageBase.install(self):
